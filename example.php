e2c6462bc04172b9ee61fc9764ecdd8c789ebdc1
MrKeliser <31136827+MrKeliser@users.noreply.github.com>
2017-11-18 09:30:26 +0300

    Create example.php
    The file shows an example of how a class works in a PHP code.

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+require 'database.php';
+$database = new DataBase("localhost", "root", "yourpassword", "yourdatabase");
+
+# Example for select array with ID:
+$id = 1;
+$dat### a = $database->select("SELECT * FROM `table` WHERE `id` = {?}", [$id]);
+
+# Example for select row with login:
+$login = "MrKeliser";
+$cell = $database->selectCell("SELECT `id` FROM `table` WHERE `login` = {?}", [$login]);
+
+# Example for query (INSERT or DELETE),
+# By the way, when you perform this action, for example insert, will return id of the inserted record.
+$id = 1;
+$login = "MrKeliser"
+$insert = $database->query("INSERT INTO `test` (`id`, `login`) VALUES ({?}, {?})", [$id, $login]);
+?>
