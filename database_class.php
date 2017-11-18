50424ca003596624cc09f8346d444f24137c85a6
MrKeliser <31136827+MrKeliser@users.noreply.github.com>
2017-11-18 09:31:57 +0300

    Create database_class.php
    The main class for working with MySQL.

---
+++ database_class.php
@@ -0,0 +1,65 @@
+<?php
+class DataBase {
+  private $mysqli;
+  private $sym_query = "{?}";
+
+  public function __construct($DBhost, $DBuser, $DBpass, $DBname) {
+    $this->mysqli = new MySQLi($DBhost, $DBuser, $DBpass, $DBname);
+    $this->mysqli->query("SET lc_time_names = 'ru_RU'");
+    $this->mysqli->query("SET NAMES 'utf8'");
+  }
+
+  private function getQuery($query, $params) {
+    if($params) {
+      for ($i = 0; $i < count($params); $i++) {
+        $pos = strpos($query, $this->sym_query);
+        $arg = "'".$this->mysqli->real_escape_string($params[$i])."'";
+        $query = substr_replace($query, $arg, $pos, strlen($this->sym_query));
+      }
+    }
+    return $query;
+  }
+
+  public function select($query, $params = false) {
+    $result_set = $this->mysqli->query($this->getQuery($query, $params));
+    if (!$result_set) return false;
+    return $this->resultSetToArray($result_set);
+  }
+
+  public function selectRow($query, $params = false) {
+    $result_set = $this->mysqli->query($this->getQuery($query, $params));
+    if ($result_set->num_rows != 1) return false;
+    else return $result_set->fetch_assoc();
+  }
+
+  public function selectCell($query, $params = false) {
+    $result_set = $this->mysqli->query($this->getQuery($query, $params));
+    if ((!$result_set) || ($result_set->num_rows != 1)) return false;
+    else {
+      $arr = array_values($result_set->fetch_assoc());
+      return $arr[0];
+    }
+  }
+
+  public function query($query, $params = false) {
+    $success = $this->mysqli->query($this->getQuery($query, $params));
+    if ($success) {
+      if ($this->mysqli->insert_id === 0) return true;
+      else return $this->mysqli->insert_id;
+    }
+    else return false;
+  }
+
+  private function resultSetToArray($result_set) {
+    $array = array();
+    while (($row = $result_set->fetch_assoc()) != false) {
+      $array[] = $row;
+    }
+    return $array;
+  }
+
+  public function __destruct() {
+    if ($this->mysqli) $this->mysqli->close();
+  }
+}
+?>
